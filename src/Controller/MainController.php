03e7f7216f80f984708dc642479ad08b4b2ad070
YzRequiem <mxmleosallier@gmail.com>
2022-03-11 14:06:41 +0100

    deploiement

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findAll();
